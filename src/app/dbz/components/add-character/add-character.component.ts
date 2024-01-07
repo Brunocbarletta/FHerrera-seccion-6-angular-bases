0248f3cc41e5ceb2b6beafd8574d153c9140d008
Bruno Cesar Barletta <brunocbarletta@gmail.com>
2024-01-07 15:38:39 -0300

    Clase 67 @output y EventEmitter + objeto emitido como copia por valor

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-add-character',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AddCharacter implements OnInit {
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name: "",
+    power: 0
+  };
+  
   constructor() { }
 
   ngOnInit() { }
+
+  emitCharacter(): void {
+    if (this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit({...this.character}); 
+
+    this.character.name = "";
+    this.character.power = 0;
+  }
 }
